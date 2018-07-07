dd82c1d47e43899377f65f86109fc7cc2be88bfa
S Arashi <34376591+AstraySteve@users.noreply.github.com>
2018-07-07 13:59:59 -0400

    attempt at json maze, untested

--- maze.js
+++ maze.js
@@ -1,8 +1,49 @@
 var fs = require('fs');
+var db = require('./db');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+var port = 3000; //could use 8080, 5000, these are open ports
+var connection;
+
+app.use(bodyParser.json());
+
+app.get('/', (request, response)=>{
+    response.json(JSON.stringify(readMap()));
+})
+
+app.listen(port, ()=> {
+    console.log(`Connected on port: ${port}`);
+    connection = db.login();
+    connection.connect();
+});
 
 //Global Variables
-var globalMap = []
+//var globalMap = []
 
+//Functions
+readMap =()=>{
+    fs.readFile('./map.txt', 'utf-8', (err, data) => {
+        if (err) throw err;
+        //console.log(data);
+        var t1 = data.split("\r\n");
+        //console.log(t1);
+        
+        var t2 =[]
+        for (var i=0; i<t1.length; i++){
+            var row = t1[i].split(',');
+            //console.log(row);
+            //globalMap.push(row);
+            t2.push(row);
+        }
+        //console.log(globalMap[1][5]);
+        //console.log(JSON.stringify(globalMap, null, 2));
+        return t2;
+    });
+}
+
+//TEST CODE
 //Map Object
 var map = {
     cols: 20,
@@ -13,25 +54,6 @@ var map = {
     }
 }
 
-//Functions
-readMap =()=>{
-    fs.readFile('./map.txt', 'utf-8', (err, data) => {
-        if (err) throw err;
-        //console.log(data);
-        var t1 = data.split("\r\n");
-        //console.log(t1);
-        
-        for (var i=0; i<t1.length; i++){
-            var row = t1[i].split(',');
-            //console.log(row);
-            globalMap.push(row);
-        }
-        //console.log(globalMap[1][5]);
-        console.log(JSON.stringify(globalMap, null, 2));
-    });
-}
-
-//TEST CODE
 renderMap =()=>{
     //Function to render map
     console.log(map.getTile(1,0));
@@ -51,4 +73,4 @@ renderMap =()=>{
 }
 
 //Main
-readMap();
+//readMap();

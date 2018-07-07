068d685754c7296f88ebbfaaad54708641f7542c
Michael Liu <michaelliu56@gmail.com>
2018-07-07 13:08:56 -0400

    new files from Michael

---
+++ server.js
@@ -0,0 +1,35 @@
+
+var fs = require('fs');
+var db = require('./db');
+
+var cors = require('cors');
+var path = require('path');
+//var express = require('express');
+//var app = express();
+var app = require('express')();
+
+var bodyParser = require('body-parser');
+
+
+
+var port = 3306;
+var connection;
+
+app.use(cors());
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+
+app.get('/',(request,response) => {
+    response.sendfile(path.resolve(('./index.html')));
+});
+
+app.get('/game',(request,response) => {
+    response.sendfile(path.resolve(('./game.html')));
+});
+
+
+app.get('/score',(request,response) => {
+    response.sendfile(path.resolve(('./score.html')));
+});
+
